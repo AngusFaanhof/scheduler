886855b79ce0a22df18822714f032b3b648d0289
AngusFaanhof <github@adfpm.anonaddy.com>
2023-06-02 16:38:18 +0200

    added schedule api endpoint

--- index.js
+++ index.js
@@ -10,23 +10,77 @@ const scheduleInterface = new ScheduleInterface();
 const transportInterface = new TransportInterface();
 
 scheduleApp.get('/', (req, res) => {
-	const data = scheduleInterface.getAll();
+	const scheduleData = scheduleInterface.getAll();
+
+	// return view of all data
+	res.json(scheduleData);
+});
+
+scheduleApp.get('/day', (req, res) => {
+	let timestamp = Date.now();
+
+	// add validation
+	if (req.query.timestamp)
+		timestamp = parseInt(req.query.timestamp, 10);
+
+	const date = new Date(timestamp);
+	const data = scheduleInterface.getDay(date);
+
+	// return view of data
+	res.json(data);
+});
+
+scheduleApp.get('/week', (req, res) => {
+	let timestamp = Date.now();
+
+	if (req.query.timestamp)
+		timestamp = parseInt(req.query.timestamp, 10);
+
+	const startDate = new Date(timestamp);
+
+	const endDate = new Date()
+	endDate.setDate(startDate.getDate() + 7);
+
+	const data = scheduleInterface.getRange(startDate, endDate);
 
 	res.json(data);
 });
 
-scheduleApp.get('/day', (req, res) => {
-	// add validation
-	const datestamp = parseInt(req.query.timestamp, 10);
-	const date = new Date(datestamp);
+scheduleApp.get('/month', (req, res) => {
+	let timestamp = Date.now();
 
-	const data = scheduleInterface.getDay(date);
+	if (req.query.timestamp)
+		timestamp = parseInt(req.query.timestamp, 10);
+
+	const startDate = new Date(timestamp);
+
+	const endDate = new Date()
+	endDate.setMonth(startDate.getMonth() + 1);
+
+	const data = scheduleInterface.getRange(startDate, endDate);
 
 	res.json(data);
 });
 
 app.get('/', (req, res) => {
-	  res.send('Welcome to the scheduler app!');
+	res.send('Welcome to the scheduler app!');
+});
+
+app.get('/today', async (req, res) => {
+	const scheduleData = scheduleInterface.getDay(new Date());
+
+	if (Object.keys(scheduleData).length === 0)
+		return res.send('No data for today');
+
+	const startTime = new Date(scheduleData[0].start);
+	const transportData = await transportInterface.getRoute(
+		config.from,
+		config.to,
+		startTime,
+		config.bufferTime
+	);
+
+	res.json(transportData);
 });
 
 app.use('/schedule', scheduleApp);
